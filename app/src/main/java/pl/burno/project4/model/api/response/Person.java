10b130fa1dbe3429b7d0ce43a927cb8364282cb0
burnO <bruno.wieczorek@gmail.com>
2016-07-28 12:35:38 +0200

    First half 2

--- app/src/main/java/pl/burno/project4/model/api/response/Person.java
+++ app/src/main/java/pl/burno/project4/model/api/response/Person.java
@@ -7,39 +7,8 @@ import com.google.gson.annotations.SerializedName;
 
 public class Person
 {
-
-    @SerializedName("gender")
-    @Expose
-    public String gender;
     @SerializedName("name")
     @Expose
     public Name name;
-    @SerializedName("location")
-    @Expose
-    public Location location;
-    @SerializedName("email")
-    @Expose
-    public String email;
-    @SerializedName("login")
-    @Expose
-    public Login login;
-    @SerializedName("registered")
-    @Expose
-    public Integer registered;
-    @SerializedName("dob")
-    @Expose
-    public Integer dob;
-    @SerializedName("phone")
-    @Expose
-    public String phone;
-    @SerializedName("cell")
-    @Expose
-    public String cell;
-    @SerializedName("picture")
-    @Expose
-    public Picture picture;
-    @SerializedName("nat")
-    @Expose
-    public String nat;
 
 }
